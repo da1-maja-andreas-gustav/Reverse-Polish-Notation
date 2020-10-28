e58cf62e2b10520b775d95198ff16d83389543b3
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2020-10-28 11:49:13 +0100

    Added skeleton

---
+++ src/ReversePolishCalculator.java
@@ -0,0 +1,31 @@
+// Version: 20200917
+// Handin done by:
+//   <202004107> <A. Malthe Henriksen>
+// Contributions:
+//   <A. Malthe Henriksen> <Programmed>
+
+import java.io.*;
+import java.util.*;
+public class ReversePolishCalculator {
+    // Add any private fields you might need here
+    public void push(int n) {
+        // Implement your code here to handle stack push
+    }
+
+    public void plus() {
+        // Implement your code here to pop two elements and push their sum
+    }
+
+    public void minus() {
+        // Implement your code here to pop two elements and push their difference
+    }
+
+    public void times() {
+        // Implement your code here to pop two elements and push their product
+    }
+
+    public int read() {
+        // Implement your code here to read the top element from the stack (without removing it)
+        return 42;
+    }
+}
